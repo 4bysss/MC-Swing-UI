3bb1f6f627df26f1f353b2a265fa7aa82ee9bb9f
4bysss <abel4992@gmail.com>
2023-03-10 20:47:31 +0100

    Practica 3 arreglada e interfaz P4 hecha.

--- P4/ca1DSim.java
+++ P4/ca1DSim.java
@@ -1,11 +1,15 @@
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JSlider;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 import javax.swing.SwingWorker;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import java.awt.image.BufferedImage;
 import java.util.Arrays;
@@ -13,17 +17,31 @@ import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+
+
+
+
+
 
 public class ca1DSim {
     private JFrame frame;
     private SpringLayout layout;
     private JComboBox<String> colores;
     private JComboBox<String> modoInicial;
+    private JComboBox<String> CondicionF;
     private JTextField generaciones, clave;
     private JButton generar,reset;
+    private JSlider numVen;
+    private JTextField valorVec;
+    private JTextField celulaInicialIn;
     private BufferedImage output;
     private Graphics g;
     private JLabel contenedor;
+    private JCheckBox distHam,entrEs,entroTemp;
+    public int[] azul,rojo,verde;
     ca1DSim(){
         //Dimensiones de la ventana
         int ancho = 600;
@@ -33,6 +51,7 @@ public class ca1DSim {
         //Modos que el ususario podra seleccionar
         String[] modos = {"Central", "Aleatoria"};
         String[] tCol = {"2","3","4","5"};
+        String[] condiciones = {"Nula","Cilindrica"};
 
         //Inicializamos todos los elementos de la ventana
         layout = new SpringLayout();
@@ -47,12 +66,41 @@ public class ca1DSim {
 
         colores = new JComboBox<>(tCol);
         modoInicial = new JComboBox<>(modos);
+        CondicionF = new JComboBox<>(condiciones);
+
 
     
-        generaciones = new JTextField(9);
-        clave = new JTextField(9);
         generar = new JButton("Generar");
         reset = new JButton("Reset");
+       
+
+        distHam = new JCheckBox("Curva de distancia de Hamming",false);
+        entroTemp = new JCheckBox("Entropía especial",false);
+        entrEs = new JCheckBox("Entropía temporal de una célula",false);
+
+
+        numVen = new JSlider();
+        numVen.setMinimum(1);
+        numVen.setMaximum(100);
+
+        
+        clave = new JTextField("Clave",9);
+        valorVec = new JTextField("Vecinos",4);
+        celulaInicialIn = new JTextField("C. Inicial",5);
+
+
+
+        generaciones = new JTextField("Generaciones",9);
+        valorVec.setEditable(false);
+        //valorVec.setEnabled(false);
+
+
+        numVen.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent arg0) {
+                valorVec.setText(Integer.toString(2*numVen.getValue() + 1));
+            }
+        });
 
 
         output = new BufferedImage(ancho, alto, BufferedImage.TYPE_INT_BGR);
@@ -62,14 +110,24 @@ public class ca1DSim {
         
         contenedor = new JLabel(new ImageIcon(output));
 
+
+
+
         //Añadimos los elementos a el frame
         frame.add(colores);
         frame.add(modoInicial);
+        frame.add(CondicionF);
         frame.add(generaciones);
         frame.add(clave);
         frame.add(generar);
         frame.add(reset);
+        frame.add(numVen);
+        frame.add(valorVec);
         frame.add(contenedor);
+        frame.add(celulaInicialIn);
+        frame.add(distHam);
+        frame.add(entroTemp);
+        frame.add(entrEs);
 
         //Colocamos dentro del frame todos y cada uno de los elementos
         layout.putConstraint(SpringLayout.WEST, contenedor, 400, SpringLayout.EAST, frame);
@@ -84,23 +142,93 @@ public class ca1DSim {
         layout.putConstraint(SpringLayout.NORTH, modoInicial, 0, SpringLayout.NORTH, colores);
 
 
+        layout.putConstraint(SpringLayout.WEST,CondicionF , 100, SpringLayout.WEST, modoInicial);
+        layout.putConstraint(SpringLayout.NORTH, CondicionF, 0, SpringLayout.NORTH, modoInicial);
+
+
+        layout.putConstraint(SpringLayout.WEST, numVen, 30, SpringLayout.EAST, CondicionF);
+        layout.putConstraint(SpringLayout.NORTH, numVen, 0, SpringLayout.NORTH, CondicionF);
+
+
+        layout.putConstraint(SpringLayout.WEST, valorVec, 30, SpringLayout.EAST, numVen);
+        layout.putConstraint(SpringLayout.NORTH, valorVec, 0, SpringLayout.NORTH, numVen);
+
+
         layout.putConstraint(SpringLayout.WEST, generaciones, 0, SpringLayout.WEST, colores);
         layout.putConstraint(SpringLayout.NORTH, generaciones, -40, SpringLayout.NORTH, colores);
 
 
         layout.putConstraint(SpringLayout.WEST, clave, 100, SpringLayout.WEST, generaciones);
         layout.putConstraint(SpringLayout.NORTH, clave, 0, SpringLayout.NORTH, generaciones);
-
+        
 
         layout.putConstraint(SpringLayout.WEST, generar, 0, SpringLayout.WEST, generaciones);
         layout.putConstraint(SpringLayout.NORTH, generar, -40, SpringLayout.NORTH, generaciones);
 
 
+        layout.putConstraint(SpringLayout.WEST, celulaInicialIn, 100, SpringLayout.WEST, clave);
+        layout.putConstraint(SpringLayout.NORTH, celulaInicialIn, 0, SpringLayout.NORTH, clave);
+        
+
         layout.putConstraint(SpringLayout.WEST, reset, 0, SpringLayout.WEST, clave);
         layout.putConstraint(SpringLayout.NORTH, reset, -40, SpringLayout.NORTH, clave);
 
 
+        layout.putConstraint(SpringLayout.WEST, distHam, 0, SpringLayout.WEST, colores);
+        layout.putConstraint(SpringLayout.NORTH, distHam, 30, SpringLayout.SOUTH, colores);
+
+
+        layout.putConstraint(SpringLayout.WEST, entroTemp, 0, SpringLayout.WEST, colores);
+        layout.putConstraint(SpringLayout.NORTH, entroTemp, 60, SpringLayout.SOUTH, colores);
+
+
+        layout.putConstraint(SpringLayout.WEST, entrEs, 0, SpringLayout.WEST, colores);
+        layout.putConstraint(SpringLayout.NORTH, entrEs, 90, SpringLayout.SOUTH, colores);
+
+
         //Añadimos las funcionalidades a los botones
+        clave.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent arg0) {
+               if (clave.getText().equals("Clave")) {
+                    clave.setText("");
+               } 
+            }
+            @Override
+            public void focusLost(FocusEvent arg0) {
+               if (clave.getText().isEmpty()) {
+                    clave.setText("Clave");
+               } 
+            }
+        });
+        generaciones.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent arg0) {
+               if (generaciones.getText().equals("Generaciones")) {
+                    generaciones.setText("");
+               } 
+            }
+            @Override
+            public void focusLost(FocusEvent arg0) {
+               if (generaciones.getText().isEmpty()) {
+                    generaciones.setText("Generaciones");
+               } 
+            }
+        });
+        celulaInicialIn.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent arg0) {
+               if (celulaInicialIn.getText().equals("C. Inicial")) {
+                    celulaInicialIn.setText("");
+               } 
+            }
+            @Override
+            public void focusLost(FocusEvent arg0) {
+               if (celulaInicialIn.getText().isEmpty()) {
+                    celulaInicialIn.setText("C. Inicial");
+               } 
+            }
+        });
         reset.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
@@ -112,12 +240,16 @@ public class ca1DSim {
 
 
             public void actionPerformed(ActionEvent arg0) {
+                boolean modoFront;
                 int[]arr1 = new int[ancho];
                 int[]arr2 = new int[ancho];
                 String mCol = (String)colores.getSelectedItem();
                 String mIni = (String)modoInicial.getSelectedItem();
+                String mFront = (String)CondicionF.getSelectedItem();
                 int generations = Integer.parseInt(generaciones.getText());
-                int key = Integer.parseInt(clave.getText());
+                long key = Long.parseLong(clave.getText());
+                int Rvecinos = numVen.getValue();
+                int Tvecinos = 2 * Rvecinos + 1;
                 switch (mIni) {
                     case "Central":
                         generaCentral(arr1,Integer.parseInt(mCol)-1);
@@ -129,19 +261,20 @@ public class ca1DSim {
                     default:
                         break;
                 }
-                if(key>Math.pow(Integer.parseInt(mCol),3*Integer.parseInt(mCol)-2)-1){
+                if(mFront == "Nula"){modoFront = false;}
+                else{modoFront = true;}
+                if(key>Math.pow(Integer.parseInt(mCol),Tvecinos*Integer.parseInt(mCol)-2*Rvecinos)-1){
                     //TODO mensaje de error
-                    System.out.println(key+"error"+(Math.pow(Integer.parseInt(mCol),3*Integer.parseInt(mCol)-2)-1));
                 }
                 else{
                     SwingWorker worker;
-                    int[] claveBin;
+                    long[] claveBin;
 
-                    claveBin = new int[3*(Integer.parseInt(mCol))-2];
+                    claveBin = new long[Tvecinos*(Integer.parseInt(mCol))-2*Rvecinos];
                     conversorKesimo(key, mCol,claveBin);
                     worker = new SwingWorker(){
                         protected Object doInBackground() throws Exception {
-                            kColores(arr1, arr2, generations, g,contenedor,claveBin,Integer.parseInt(mCol));
+                            kColores(arr1, arr2, generations, g,contenedor,claveBin,Integer.parseInt(mCol),modoFront, Rvecinos);
                             return null;
                         };
                     };
@@ -155,9 +288,9 @@ public class ca1DSim {
         
     }
     
-    private void conversorKesimo(int clave, String nCol,int[] inputOutput){
-        int[] Arraux = new int[inputOutput.length];
-        int aux;
+    private void conversorKesimo(long clave, String nCol,long[] inputOutput){
+        long[] Arraux = new long[inputOutput.length];
+        long aux;
         int i;
         switch (nCol) {
             case "2":
@@ -175,10 +308,8 @@ public class ca1DSim {
 
                 }
                 for (int j = 0; j < Arraux.length; j++) {
-                    inputOutput[j]=Arraux[Arraux.length - j - 1];
+                    inputOutput[j]=Arraux[j];
                 }
-                System.out.println(Arrays.toString(inputOutput));
-                System.out.println(Arrays.toString(Arraux));
                 break;
 
             case "3":
@@ -198,8 +329,6 @@ public class ca1DSim {
                 for (int j = 0; j < Arraux.length; j++) {
                     inputOutput[j]=Arraux[j];
                 }
-                System.out.println(Arrays.toString(inputOutput));
-                System.out.println(Arrays.toString(Arraux));
                 break;
             case "4":
                 aux = clave;
@@ -216,10 +345,8 @@ public class ca1DSim {
 
                 }
                 for (int j = 0; j < Arraux.length; j++) {
-                    inputOutput[j]=Arraux[Arraux.length - j - 1];
+                    inputOutput[j]=Arraux[j];
                 }
-                System.out.println(Arrays.toString(inputOutput));
-                System.out.println(Arrays.toString(Arraux));
                 break;
             case "5":
                 aux = clave;
@@ -238,8 +365,6 @@ public class ca1DSim {
                 for (int j = 0; j < Arraux.length; j++) {
                     inputOutput[j]=Arraux[j];
                 }
-                System.out.println(Arrays.toString(inputOutput));
-                System.out.println(Arrays.toString(Arraux));
                 break;
         }
     }
@@ -263,25 +388,35 @@ public class ca1DSim {
     }
 
 
-    private void kColores(int[]arr1,int[]arr2,int generaciones,Graphics g,JLabel contenedor,int[]claveBin,int nCol){
+    private void kColores(int[]arr1,int[]arr2,int generaciones,Graphics g,JLabel contenedor,long[]claveBin,int nCol,boolean cFront,int Rvecinos){
         int[] temp = new int[arr1.length];
-        int suma;
+        int suma = 0;
         System.out.println(Arrays.toString(claveBin));
-        System.out.println(Arrays.toString(arr1));
         for (int i = 0; i < generaciones; i++) {
             int cosa = 0;
             for (int j = 0; j < arr1.length; j++) {
-
-                if(j == 0){ suma = (arr1[j]+arr1[j+1]+arr1[arr1.length-1]);}
-
-                else if(j == arr1.length-1){ suma = (arr1[j]+arr1[0]+arr1[j-1]);}
-
-                else{ suma = arr1[j] + arr1[j+1] + arr1[j-1];
-                    System.out.println(suma +"="+arr1[j]+"+"+arr1[j+1]+"+"+arr1[j-1]);}
-                arr2[j] = claveBin[suma];
-                suma =0 ;
+                if(cFront){
+                    for (int k = 0; k <= Rvecinos; k++) {
+                        suma += arr1[(k+j)%arr1.length];
+                    }
+                    for(int k=1,aux = 0;k <= Rvecinos && aux < Rvecinos ;k++,aux++){
+                        if(j-k<0){
+                            k = -(arr1.length - j - 1);
+                        }
+                        suma += arr1[j-k];
+                    }
+                }
+                else{
+                    for (int k = 0; k <= Rvecinos && k+j < arr1.length; k++) {
+                        suma += arr1[k+j];
+                    }
+                    for(int k=1;k <= Rvecinos && j-k>=0;k++){
+                        suma += arr1[j-k];
+                    }
+                }
+                arr2[j] = (int)claveBin[suma];
+                suma = 0;
             }
-        System.out.println(Arrays.toString(arr2));
             for (int k = 0; k < arr2.length; k++) {
                 switch (arr2[k]) {
                     case 0:
@@ -318,7 +453,6 @@ public class ca1DSim {
             }*/
             contenedor.repaint();
         }
-        System.out.println("Dibujo!");
     }
 
 
